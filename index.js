9b5100f43f8e004ccb59a921f4262db8a4100572
Georgiy Kuznetsov <georgijkuznecov933@gmail.com>
2023-12-27 21:29:38 +0300

    00

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+export const multiplyTwoNumbers = (a, b) => {
+    return a * b;
+}
 
+export const getMaxNumber = (a, b) => {
+    return Math.max(a, b);
+}
+
+export const castReversedBoolean = (value) => {
+    return !Boolean(value);
+}
+
+export const determineEntrance = (num) => {
+    if (num >= 1 && num <= 20) {
+        return 1;
+      } else if (num >= 21 && num <= 48) {
+        return 2;
+      } else if (num >= 49 && num <= 90) {
+        return 3;
+      }
+      return 0;
+}
+
+export const getSum = (num) => {
+    return num
+      .toString()
+      .split('')
+      .map(Number)
+      .reduce((a, b) => a + b, 0);
+}
